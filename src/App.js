81604efc732f9595b889f60cd0c18ff33bc743b5
EvgeniiZhukov <ev.zhykov@gmail.com>
2025-12-04 13:03:18 +0300

    Another misprint

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
 import { Shop } from "./components/Shop";
-import { ContextProvider } from "./contex";
+import { ContextProvider } from "./context";
 
 function App() {
   return (
